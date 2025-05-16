529b24cd06ee9a1d5c8ff0baa883c38e01c6aeaf
Firtness <u202122484@upc.edu.pe>
2025-05-16 13:15:36 -0500

    fix(router-error): fix error

--- src/app/challenges/components/challenge-card-item/challenge-card-item.component.ts
+++ src/app/challenges/components/challenge-card-item/challenge-card-item.component.ts
@@ -1,14 +1,19 @@
 import {Component, Input} from '@angular/core';
-import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {MatCard, MatCardContent, MatCardFooter, MatCardHeader} from "@angular/material/card";
 import {Challenge} from "../../model/challenge.entity";
+import {RouterLink} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-challenge-card-item',
-  imports: [
-    MatCardHeader,
-    MatCardContent,
-    MatCard
-  ],
+    imports: [
+        MatCardHeader,
+        MatCardContent,
+        MatCard,
+        MatCardFooter,
+        RouterLink,
+        MatButton
+    ],
   templateUrl: './challenge-card-item.component.html',
   styleUrl: './challenge-card-item.component.css'
 })
