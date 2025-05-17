419bd6f95e4d0d7e9a95032cc5fb66aa00fb7033
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-05-16 20:09:48 -0500

    feat: modified environments to use deployed fake api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverBaseUrl: 'http://localhost:3000/api/v1',
+  serverBaseUrl: 'https://eduhivefakeapi.onrender.com/api/v1',
   groupsEndpointPath: '/groups',
   groupJoinCodesEndpointPath: '/groupJoinCodes',
   usersEndpointPath: '/users',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'http://localhost:3000/api/v1',
+  serverBaseUrl: 'https://eduhivefakeapi.onrender.com/api/v1',
   groupsEndpointPath: '/groups',
   groupJoinCodesEndpointPath: '/groupJoinCodes',
   usersEndpointPath: '/users',
