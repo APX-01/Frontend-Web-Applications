5246801958a7bf13bc1f007a7f53c28fd59bb601
sebastianrealcalderon <u20221d964@upc.edu.pe>
2025-07-12 04:40:04 -0500

    feat: user score

--- src/app/group/components/group-item/group-item.component.ts
+++ src/app/group/components/group-item/group-item.component.ts
@@ -12,6 +12,9 @@ import { MatButton, MatIconButton } from "@angular/material/button";
 import { ProfileInGroup } from "../../../iam/model/profile-in-group.entity";
 import { RouterLink } from "@angular/router";
 import { MatIcon } from "@angular/material/icon";
+import {AuthService} from "../../../iam/services/auth.service";
+import {SubmissionApiService} from "../../../challenges/services/submission-api.service";
+import {User} from "../../../iam/model/user.entity";
 
 @Component({
     selector: 'app-group-item',
@@ -35,7 +38,38 @@ export class GroupItemComponent implements OnInit {
     @Input() group: Group = new Group({});
     @Input() groupProfile: ProfileInGroup = new ProfileInGroup({});
 
-    ngOnInit(): void {
+    user: User | null = new User({})
+    userScore: number = 0;
 
+    constructor(
+        private authService: AuthService,
+        private submissionService: SubmissionApiService
+    ) {}
+
+
+    ngOnInit(): void {
+        this.user = this.authService.getUser();
+
+        if (this.user) {
+            this.getUserScore(this.user).then(r => {this.userScore = r})
+        }
     }
+
+    async getUserScore(user: User): Promise<number> {
+        try {
+            const submissions = await this.submissionService
+                .getSubmissionsByStudentIdAndGroupId(user.id, this.group.id)
+                .toPromise();
+
+            if (!submissions) {
+                return 0;
+            }
+
+            return submissions.reduce((total, submission) => total + (submission.score || 0), 0);
+        } catch (error) {
+            console.error('Error fetching submissions:', error);
+            return 0;
+        }
+    }
+
 }
